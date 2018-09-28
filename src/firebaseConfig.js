3e1ee03a873b1898cdabb171338f4f3fa7d2e856
akeshavan <keshavan@berkeley.edu>
2018-09-28 11:43:38 -0700

    fix: added firebaseConfig file back into the mix

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,8 @@
 import firebase from 'firebase';
+import config from './config';
 
-// copy/paste this from your firebase console
-var config = {
-  apiKey: "AIzaSyCNLq_efw3EQLSJi1H9bFEwkkmvggrfoYI",
-  authDomain: "braindr-les.firebaseapp.com",
-  databaseURL: "https://braindr-les.firebaseio.com",
-  projectId: "braindr-les",
-  storageBucket: "braindr-les.appspot.com",
-  messagingSenderId: "102313057158"
-};
-firebase.initializeApp(config);
+firebase.initializeApp(config.firebaseKeys);
 
+/* eslint-disable */
 export const db = firebase.database();
+/* eslint-enable */
